1c52e816d61821822d67aa404d849b3b28e6cbcd
james901004 <jameswu123@gwmail.gwu.edu>
2016-06-17 14:58:29 -0700

    Update control.py

--- control.py
+++ control.py
@@ -1,1 +1,12 @@
 
+
+# import modules
+import time 
+import RPi.GPIO as GPIO
+
+#set up pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(12,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
