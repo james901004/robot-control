f70ca3090e137ad5d718e64be5a9c1dab4172477
james901004 <jameswu123@gwmail.gwu.edu>
2016-06-20 15:07:28 -0700

    Update control.py

--- control.py
+++ control.py
@@ -3,44 +3,11 @@ import time
 import RPi.GPIO as GPIO
 
 #set up pins
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
 GPIO.setup(12,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
 
-
-while true:
-  try:
-    bottns=
-    
-    # go forward
-    if(bottons & #forward)
-    GPIO.output(11, True)
-    GPIO.output(12, True)
-    GPIO.output(13, False)
-    GPIO.output(15, False)
-    time.sleep(3)
-    
-    # go left
-    if(bottons & #left)
-    GPIO.output(11, False)
-    GPIO.output(12, True)
-    GPIO.output(13, False)
-    GPIO.output(15, False)
-    time.sleep(3)
-    
-    #go right
-    if(bottons & #right)
-    GPIO.output(11, True)
-    GPIO.output(12, False)
-    GPIO.output(13, False)
-    GPIO.output(15, False)
-    time.sleep(3)
-    
-    #go backward
-    GPIO.output(11, True)
-    GPIO.output(12, True)
-    GPIO.output(13, True)
-    GPIO.output(15, True)
-    time.sleep(3)
+p1 = GPIO.PWM(11,1000)
+P1.start(10)
