eafed281dc3d82963fae4f8230c15b0d06c9c438
james901004 <jameswu123@gwmail.gwu.edu>
2016-07-29 11:45:06 -0700

    Update test.py

--- test.py
+++ test.py
@@ -32,30 +32,32 @@ GPIO.output(GPIO_TRIGGER, False)
 
 # Allow module to settle
 time.sleep(0.5)
-
+i=1
+while i<100
+ i=i+1
 # Send 10us pulse to trigger
-GPIO.output(GPIO_TRIGGER, True)
-time.sleep(0.00001)
-GPIO.output(GPIO_TRIGGER, False)
-start = time.time()
+ GPIO.output(GPIO_TRIGGER, True)
+ time.sleep(0.00001)
+ GPIO.output(GPIO_TRIGGER, False)
+ start = time.time()
 
-while GPIO.input(GPIO_ECHO)==0:
+ while GPIO.input(GPIO_ECHO)==0:
   start = time.time()
 
-while GPIO.input(GPIO_ECHO)==1:
+ while GPIO.input(GPIO_ECHO)==1:
   stop = time.time()
 
 # Calculate pulse length
-elapsed = stop-start
+ elapsed = stop-start
 
 # Distance pulse travelled in that time is time
 # multiplied by the speed of sound (cm/s)
-distance = elapsed * 34300
+ distance = elapsed * 34300
 
 # That was the distance there and back so halve the value
-distance = distance / 2
+ distance = distance / 2
 
-print "Distance : %.1f" % distance
+ print "Distance : %.1f" % distance
 
 # Reset GPIO settings
 GPIO.cleanup()
